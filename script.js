01d0fdea649c042b7fd9785f0479db01cf15c831
Kathrine <kath423a@stud.kea.dk>
2021-02-09 09:52:18 +0100

    basic kode tilføjet

---
+++ script.js
@@ -0,0 +1,11 @@
+"use script";
+
+window.addEventListener("DOMContentLoaded", sidenVises);
+
+function sidenVises() {
+  console.log("sidenVises, Start");
+
+  document.querySelector("button").addEventListener("click", showOutput);
+}
+
+function showOutput() {}
