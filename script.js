da12b4f03473f10491f5b01068a9f73ec63c6394
Kathrine <kath423a@stud.kea.dk>
2021-02-09 10:10:29 +0100

    peters solution added

--- script.js
+++ script.js
@@ -8,4 +8,20 @@ function sidenVises() {
   document.querySelector("button").addEventListener("click", showOutput);
 }
 
-function showOutput() {}
+function showOutput() {
+  console.log("showOutput");
+
+  //read userInput
+}
+
+// Hyphen vist af Peter
+const fullName = "justin flinch-fletchley";
+
+const firstSpace = fullName.indexOf(" ");
+
+const beforePart = fullName.substring(0, firstSpace);
+const character = fullName[firstSpace + 1];
+const afterPart = fullName.substring(firstSpace + 2);
+
+const capitalized = beforePart + character.toUpperCase() + afterPart;
+console.log(`Capitalized name: ${capitalized}`);
